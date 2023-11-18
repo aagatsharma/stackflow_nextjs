49cde4c1669cc6efdb07c511e03e8538a72ad62d
aagatsharma <aagats72@gmail.com>
2023-11-18 14:42:45 +0545

    get users name and profile to questioncard

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -42,19 +42,22 @@ export default async function Home() {
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {result.questions.length > 0 ? (
-          result.questions.map((question) => (
-            <QuestionCard
-              key={question._id}
-              _id={question._id}
-              title={question.title}
-              tags={question.tags}
-              author={question.author}
-              upvotes={question.upvotes}
-              views={question.views}
-              answers={question.answers}
-              createdAt={question.createdAt}
-            />
-          ))
+          result.questions.map((question) => {
+            console.log(question);
+            return (
+              <QuestionCard
+                key={question._id}
+                _id={question._id}
+                title={question.title}
+                tags={question.tags}
+                author={question.author[0]}
+                upvotes={question.upvotes}
+                views={question.views}
+                answers={question.answers}
+                createdAt={question.createdAt}
+              />
+            );
+          })
         ) : (
           <NoResult
             title="There's no question to show"
--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -47,8 +47,7 @@ const QuestionCard = ({
       </div>
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
         <Metric
-          //   imgUrl={author.picture}
-          imgUrl="/assets/icons/avatar.svg"
+          imgUrl={author.picture}
           alt="User"
           value={author.name}
           title={` - asked ${getTimestamp(createdAt)}`}
